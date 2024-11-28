8bcefeb1a3edb0a9fc0f2145a93b629d25b6a508
Amanda Lima Mota <amandalimamotta12@gmail.com>
2024-11-28 14:29:24 -0300

    model criada

---
+++ lib/app/data/models/produto_model.dart
@@ -0,0 +1,20 @@
+class ProdutoModel {
+  final String title;
+  final String description;
+  final String price;
+  final String ratinge;
+  final String brand;
+  final String category;
+  final String thumbnail;;
+  final String images;
+
+  ProdutoModel({required this.title,
+    required this.description,
+    required this.price,
+    required this.ratinge,
+    required this.brand,
+    required this.category,
+    required this.thumbnail,
+    required this.images
+  });
+}
