fc9a2ae0cbe15ec0447ae6c587ca87d711a49bc0
Amanda Lima Mota <amandalimamotta12@gmail.com>
2024-12-02 17:01:38 -0300

    repositorio feito, e tratamento de erro

--- lib/app/data/repositories/produto_repository.dart
+++ lib/app/data/repositories/produto_repository.dart
@@ -1,10 +1,41 @@
- abstract class IProdutoReposity {
-  Future<List<ProductModel>> getProdutos();
+import 'dart:convert';
+
+import 'package:descritivo_paradas/app/data/http/exceptions.dart';
+import 'package:descritivo_paradas/app/data/models/produto_model.dart';
+
+abstract class IProdutoReposity {
+  Future<List<ProdutoModel>> getProdutos();
  }
 
- class ProdutoRepository implements IProdutoRepositoty {
+ class ProdutoRepository implements IProdutoReposity {
+   final IHttpClientclient;
+
+  ProdutoRepository({required this.client});
+
+  
+
   @override
-  Future<List<ProdutoModel>> getProdutos() {
-    throw UnimplementedError()
+  Future<List<ProdutoModel>> getProdutos() async {
+    final response = await client.get(
+      url: "https://dummyjson.com/products" //link da https aqui
+    );
+
+    if (response.satusCode == 200) {
+      final List<ProdutoModel> produtos = [];
+
+      //transforma a string em map
+      final body = jsonDecode(response.body);
+
+      body['products'].map((item) {
+        final ProdutoModel produto = ProdutoModel.fromMap(item);
+        produtos. add(produto);
+      }).toList();     
+
+      return produtos;
+    } else if(response.satatusCode == 404) {
+      throw NotFoundException('A url não é válida');
+    } else {
+      throw Exception('Não foi possível carregar os produtos');
+    }
   }
- }
+}
