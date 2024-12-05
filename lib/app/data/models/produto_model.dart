eeaf3cb48905643b218bd9225250ebab9f7c3d85
Amanda Lima Mota <amandalimamotta12@gmail.com>
2024-12-05 16:52:41 -0300

    cod revisado2

--- lib/app/data/models/produto_model.dart
+++ lib/app/data/models/produto_model.dart
@@ -1,34 +1,34 @@
 class ProdutoModel {
   final String title;
   final String description;
-  final String price;
-  final String ratinge;
+  final double price;
+  final double rating;
   final String brand;
   final String category;
   final String thumbnail;
-  final String images;
+  final List<String> images;
 
-  ProdutoModel({required this.title,
+  ProdutoModel({
+    required this.title,
     required this.description,
     required this.price,
-    required this.ratinge,
+    required this.rating,
     required this.brand,
     required this.category,
     required this.thumbnail,
-    required this.images
+    required this.images,
   });
 
-  factory  ProdutoModel.fromMap(Map<String, dynamic> map) {
-    return ProdutoModel( 
+  factory ProdutoModel.fromMap(Map<String, dynamic> map) {
+    return ProdutoModel(
       title: map['title'],
       description: map['description'],
-      price:  map['price']  * 1.0,
-      ratinge:  map['ratinge'] * 1.0,
-      brand:  map['brand'],
-      category:  map['category'],
-      thumbnail:  map['thumbnail'],
+      price: map['price'] * 1.0,
+      rating: map['rating'] * 1.0,
+      brand: map['brand'],
+      category: map['category'],
+      thumbnail: map['thumbnail'],
       images: List<String>.from((map['images'] as List)),
     );
   }
 }
- 
--- lib/app/data/repositories/produto_repository.dart
+++ lib/app/data/repositories/produto_repository.dart
@@ -1,39 +1,36 @@
 import 'dart:convert';
-
 import 'package:descritivo_paradas/app/data/http/exceptions.dart';
+import 'package:descritivo_paradas/app/data/http/http_client.dart';
 import 'package:descritivo_paradas/app/data/models/produto_model.dart';
 
 abstract class IProdutoReposity {
   Future<List<ProdutoModel>> getProdutos();
- }
+}
 
- class ProdutoRepository implements IProdutoReposity {
-   final IHttpClientclient;
+class ProdutoRepository implements IProdutoReposity {
+  final IHttpClient client;
 
   ProdutoRepository({required this.client});
-
-  
 
   @override
   Future<List<ProdutoModel>> getProdutos() async {
     final response = await client.get(
-      url: "https://dummyjson.com/products" //link da https aqui
+      url: 'https://dummyjson.com/products',
     );
 
-    if (response.satusCode == 200) {
+    if (response.statusCode == 200) {
       final List<ProdutoModel> produtos = [];
 
-      //transforma a string em map
       final body = jsonDecode(response.body);
 
       body['products'].map((item) {
         final ProdutoModel produto = ProdutoModel.fromMap(item);
-        produtos. add(produto);
-      }).toList();     
+        produtos.add(produto);
+      }).toList();
 
       return produtos;
-    } else if(response.satatusCode == 404) {
-      throw NotFoundException('A url não é válida');
+    } else if (response.statusCode == 404) {
+      throw NotFoundException('A url informada não é válida');
     } else {
       throw Exception('Não foi possível carregar os produtos');
     }
